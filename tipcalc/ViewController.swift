2657a97517de9a4cc91c98e21071d02199969ae1
Projects <projects@mantaqaas-mbp.fios-router.home>
2019-01-05 19:03:41 -0500

    Initial Commit

---
+++ tipcalc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tipcalc
+//
+//  Created by Projects on 1/5/19.
+//  Copyright © 2019 Projects. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
